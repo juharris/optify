4a3f825a15063f0b8cb447808f7158e8cc904058
Justin Harris <juharris@users.noreply.github.com>
2025-02-03 10:09:00 -0500

    CI: Show gem file size

--- ruby/optify/lib/optify.rb
+++ ruby/optify/lib/optify.rb
@@ -4,8 +4,8 @@
 # The implementation to use directly Ruby and with types declared.
 require_relative "optify_ruby/implementation"
 
-puts "Loading Optify bindings. Warnings about redefining methods are normal and can be ignored because the implementations in Ruby are not implemented and only exist to help with type checking."
 
 # The implementation in Rust which redefines some methods.
 # This yields some warnings, but we should redeclare the methods in Ruby to help with type checking anyway.
+# Warnings about redefining methods are normal and can be ignored because the implementations in Ruby are not implemented and only exist to help with type checking.
 require_relative "optify_ruby/optify_ruby"
