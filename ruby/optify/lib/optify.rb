6c7050458e0c0a0100a86ee64807a940a3ca387e
Justin Harris <juharris@users.noreply.github.com>
2025-02-10 11:13:33 -0500

    [ruby] Set ruby version sub-dir

--- ruby/optify/lib/optify.rb
+++ ruby/optify/lib/optify.rb
@@ -8,6 +8,19 @@ require_relative 'optify_ruby/implementation'
 # This yields some warnings, but we should redeclare the methods in Ruby to help with type checking anyway.
 # Warnings about redefining methods are normal and can be ignored
 # because the implementations in Ruby are not implemented and only exist to help with type checking.
-require_relative 'optify_ruby/optify_ruby'
+# Ideally we do:
+# `require_relative 'optify_ruby/optify_ruby'`
+# but that doesn't work when building for multiple versions of Ruby.
+# So we have to do this which is similar to something from 'https://github.com/matsadler/halton-rb/blob/main/lib/halton.rb'.
+begin
+  /(?<ruby_version>\d+\.\d+)/ =~ RUBY_VERSION
+  require_relative "optify_ruby/#{ruby_version}/optify_ruby"
+rescue LoadError
+  begin
+    require_relative 'optify_ruby/optify_ruby'
+  rescue LoadError # Cargo Builder in RubyGems < 3.4.6 doesn't install to dir
+    require_relative 'optify_ruby.so'
+  end
+end
 
 require_relative 'optify_ruby/base_config'
