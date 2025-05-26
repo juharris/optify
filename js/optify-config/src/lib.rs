7d8f8c6742e3ca1883d274dac4b43b15cbd2a74c
Justin Harris <juharris@users.noreply.github.com>
2025-05-26 09:58:03 -0400

    JS: Add fmt check

--- js/optify-config/src/lib.rs
+++ js/optify-config/src/lib.rs
@@ -1,8 +1,8 @@
 #![deny(clippy::all)]
 
 use optify::builder::{OptionsProviderBuilder, OptionsRegistryBuilder};
-use optify::provider::{OptionsProvider, OptionsRegistry};
 use optify::convert_to_str_slice;
+use optify::provider::{OptionsProvider, OptionsRegistry};
 
 #[macro_use]
 extern crate napi_derive;
@@ -15,22 +15,26 @@ pub struct JsOptionsProvider {
 #[napi]
 impl JsOptionsProvider {
   #[napi(constructor)]
-    pub fn new() -> Self {
-        Self { inner: None }
-    }
+  pub fn new() -> Self {
+    Self { inner: None }
+  }
 
-    #[napi]
-    pub fn features(&self) -> Vec<String> {
-        self.inner.as_ref().unwrap().get_features()
-    }
+  #[napi]
+  pub fn features(&self) -> Vec<String> {
+    self.inner.as_ref().unwrap().get_features()
+  }
 
-    #[napi]
-    pub fn get_options_json(&self, key: String, feature_names: Vec<String>) -> napi::Result<String> {
-        let feature_names = convert_to_str_slice!(feature_names);
-        self.inner.as_ref().unwrap().get_options(&key, &feature_names)
-            .map(|json| json.to_string())
-            .map_err(|e| napi::Error::from_reason(e.to_string()))
-    }
+  #[napi]
+  pub fn get_options_json(&self, key: String, feature_names: Vec<String>) -> napi::Result<String> {
+    let feature_names = convert_to_str_slice!(feature_names);
+    self
+      .inner
+      .as_ref()
+      .unwrap()
+      .get_options(&key, &feature_names)
+      .map(|json| json.to_string())
+      .map_err(|e| napi::Error::from_reason(e.to_string()))
+  }
 }
 
 #[napi(js_name = "OptionsProviderBuilder")]
@@ -40,23 +44,31 @@ pub struct JsOptionsProviderBuilder {
 
 #[napi]
 impl JsOptionsProviderBuilder {
-    #[napi(constructor)]
-    pub fn new() -> Self {
-        Self {
-            inner: OptionsProviderBuilder::new(),
-        }
+  #[napi(constructor)]
+  pub fn new() -> Self {
+    Self {
+      inner: OptionsProviderBuilder::new(),
     }
+  }
 
-    #[napi]
-    pub fn add_directory(&mut self, directory: String) -> napi::Result<()> {
-        let path = std::path::Path::new(&directory);
-        self.inner.add_directory(path).map_err(|e| napi::Error::from_reason(e.to_string()))?;
-        Ok(())
-    }
+  #[napi]
+  pub fn add_directory(&mut self, directory: String) -> napi::Result<()> {
+    let path = std::path::Path::new(&directory);
+    self
+      .inner
+      .add_directory(path)
+      .map_err(|e| napi::Error::from_reason(e.to_string()))?;
+    Ok(())
+  }
 
-    #[napi]
-    pub fn build(&mut self) -> napi::Result<JsOptionsProvider> {
-        let provider = self.inner.build().map_err(|e| napi::Error::from_reason(e.to_string()))?;
-        Ok(JsOptionsProvider { inner: Some(provider) })
-    }
+  #[napi]
+  pub fn build(&mut self) -> napi::Result<JsOptionsProvider> {
+    let provider = self
+      .inner
+      .build()
+      .map_err(|e| napi::Error::from_reason(e.to_string()))?;
+    Ok(JsOptionsProvider {
+      inner: Some(provider),
+    })
+  }
 }
