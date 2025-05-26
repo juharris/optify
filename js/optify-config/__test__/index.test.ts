3e77bb7343ece9f8838c331e9d2bbb4194ba8527
Justin Harris <juharris@users.noreply.github.com>
2025-05-25 21:52:08 -0400

    Start bindings

--- js/optify-config/__test__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import {describe, expect, test} from '@jest/globals';
-import { sum } from "../index.js";
-
-describe('sum function', () => {
-  test('should add two numbers correctly', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
