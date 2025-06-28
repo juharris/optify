6481788a29dac4979d3912298a435c4f9ea8ce87
Justin Harris <juharris@users.noreply.github.com>
2025-06-28 17:36:51 -0400

    [Rust][tests] Dynamically make tests for suites

---
+++ rust/optify/build.rs
@@ -0,0 +1,31 @@
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+
+fn main() {
+    generate_test_suites();
+}
+
+/// Dynamically generate the tests for each folder in tests/test_suites.
+fn generate_test_suites() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("test_suites.rs");
+    let mut f = fs::File::create(&dest_path).unwrap();
+
+    for entry in fs::read_dir("../../tests/test_suites").unwrap().flatten() {
+        let path = entry.path();
+        if path.is_dir() && !path.starts_with(".") {
+            if let Some(file_name) = path.file_name() {
+                if let Some(suite_name) = file_name.to_str() {
+                    writeln!(f,
+                                "#[test]\nfn test_suite_{}() {{\n    test_suite(std::path::Path::new(\"../../tests/test_suites/{}\"));\n}}\n", 
+                                suite_name, suite_name
+                            ).unwrap();
+                }
+            }
+        }
+    }
+
+    println!("cargo:rerun-if-changed=../../tests/test_suites");
+    println!("cargo:rerun-if-changed=build.rs");
+}
--- rust/optify/tests/test_suites.rs
+++ rust/optify/tests/test_suites.rs
@@ -42,16 +42,5 @@ fn test_suite(path: &std::path::Path) {
     });
 }
 
-#[test]
-fn test_suites() {
-    let test_suites_dir = "../../tests/test_suites";
-    let entries = fs::read_dir(test_suites_dir).unwrap();
-
-    // TODO Split into the equivalent of different `#[test]` functions automatically, if possible.
-    entries.for_each(|entry| {
-        let p = entry.unwrap().path();
-        if p.is_dir() {
-            test_suite(p.as_path());
-        }
-    });
-}
+// Include the dynamically generated test functions.
+include!(concat!(env!("OUT_DIR"), "/test_suites.rs"));
