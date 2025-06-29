f06d3a67195e2db13d7ed10f5fffd959a9e39a2a
Justin D. Harris <1594505+juharris@users.noreply.github.com>
2025-06-29 12:22:18 -0400

    [JS] Add get_all_options_json (#68)
    Bump to 0.1.0

--- js/optify-config/tests/optify.test.ts
+++ js/optify-config/tests/optify.test.ts
@@ -10,24 +10,24 @@ const runSuite = (suitePath: string) => {
   const expectationsPath = path.join(suitePath, 'expectations');
   for (const testCase of fs.readdirSync(expectationsPath)) {
     const expectationPath = path.join(expectationsPath, testCase);
-    const expectedInfo = JSON.parse(fs.readFileSync(expectationPath, 'utf8'));
-    const { options: expectedOptions, features } = expectedInfo;
-    for (const [key, expectedValue] of Object.entries(expectedOptions)) {
-      const actualJson = provider.getOptionsJson(key, features);
-      const actualOptions = JSON.parse(actualJson);
-      expect(actualOptions).toEqual(expectedValue);
-    }
+    test(`${testCase}`, () => {
+      const expectedInfo = JSON.parse(fs.readFileSync(expectationPath, 'utf8'));
+      const { options: expectedOptions, features } = expectedInfo;
+      for (const [key, expectedValue] of Object.entries(expectedOptions)) {
+        const actualJson = provider.getOptionsJson(key, features);
+        const actualOptions = JSON.parse(actualJson);
+        expect(actualOptions).toEqual(expectedValue);
+      }
+    });
   }
-};
+}
 
-describe('Suites', () => {
-  const testSuitesDir = path.join(__dirname, '../../../tests/test_suites');
-  for (const suite of fs.readdirSync(testSuitesDir)) {
+const testSuitesDir = path.join(__dirname, '../../../tests/test_suites');
+for (const suite of fs.readdirSync(testSuitesDir)) {
+  describe(`Suite: ${suite}`, () => {
     const suitePath = path.join(testSuitesDir, suite);
     if (fs.statSync(suitePath).isDirectory()) {
-      test(`Suite: ${suite}`, () => {
-        runSuite(suitePath);
-      });
+      runSuite(suitePath);
     }
-  }
-});
+  });
+}
