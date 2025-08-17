9ba7ec59a03692d01312221690e1a9450b290c5e
Justin Harris <juharris@users.noreply.github.com>
2025-08-17 10:13:22 -0400

    Rust: Copy schema

--- rust/optify/build.rs
+++ rust/optify/build.rs
@@ -9,20 +9,42 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 }
 
-/// Copy the schema file to the output directory so it can be included in the crate
+/// Copy the schema file to the output directory so it can be used during the build to embed it in the crate.
 fn copy_schema_file() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = std::env::var("OUT_DIR")?;
     let dest_path = Path::new(&out_dir).join("schemas/feature_file.json");
     let dest_dir = dest_path.parent().unwrap();
     fs::create_dir_all(dest_dir)?;
 
-    let source_path = Path::new("../../schemas/feature_file.json");
-    if source_path.exists() {
-        fs::copy(source_path, &dest_path)
-            .unwrap_or_else(|e| panic!("Failed to copy schema file from {:?}: {}", source_path, e));
-        println!("cargo:rerun-if-changed={}", source_path.display());
+    // Try multiple locations for the schema file.
+    let original_path = Path::new("../../schemas/feature_file.json");
+    let copied_path = Path::new("schemas/feature_file.json");
+    let possible_paths = [
+        // Original location in repo, used when running tests.
+        original_path,
+        // A copy for `cargo publish`.
+        copied_path,
+    ];
+
+    let mut source_path = None;
+    for path in &possible_paths {
+        if path.exists() {
+            source_path = Some(path);
+            break;
+        }
+    }
+
+    if let Some(path) = source_path {
+        fs::copy(path, &dest_path)
+            .unwrap_or_else(|e| panic!("Failed to copy schema file from {:?}: {}", path, e));
+        println!("cargo:rerun-if-changed={}", path.display());
     } else {
-        panic!("Schema file not found at {:?}", source_path);
+        panic!(
+            "Schema file not found at any of the expected locations: {:?}. If you are running `cargo publish`, then the schema needs to be copied from {:?} to {:?}.",
+            possible_paths,
+            original_path,
+            copied_path
+        );
     }
 
     Ok(())
@@ -40,8 +62,8 @@ fn generate_test_suites() {
             if let Some(file_name) = path.file_name() {
                 if let Some(suite_name) = file_name.to_str() {
                     writeln!(f,
-                                "#[test]\nfn test_suite_{suite_name}() {{\n    test_suite(std::path::Path::new(\"../../tests/test_suites/{suite_name}\")).unwrap();\n}}\n"
-                            ).unwrap();
+                                    "#[test]\nfn test_suite_{suite_name}() {{\n    test_suite(std::path::Path::new(\"../../tests/test_suites/{suite_name}\")).unwrap();\n}}\n"
+                                ).unwrap();
                 }
             }
         }
