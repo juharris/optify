c32e823ede04aebdf8ab066cea3baf5915da3580
Justin Harris <juharris@users.noreply.github.com>
2025-09-27 22:05:49 -0400

    Python: handle

--- python/optify/tests/test_optify.py
+++ python/optify/tests/test_optify.py
@@ -4,10 +4,12 @@ from pathlib import Path
 
 from optify import GetOptionsPreferences, OptionsProvider, OptionsProviderBuilder
 
+
 def test_empty_build():
     builder = OptionsProviderBuilder()
     provider = builder.build()
     assert provider is not None
+
 
 def run_suite(suite_path: str) -> None:
     provider = OptionsProvider.build(os.path.join(suite_path, 'configs'))
@@ -17,24 +19,26 @@ def run_suite(suite_path: str) -> None:
         with open(expectation_path, 'r') as f:
             expected_info = json.load(f)
         constraints = expected_info.get('constraints')
+        preferences = GetOptionsPreferences()
+        preferences.enable_configurable_strings()
         if constraints is not None:
-            preferences = GetOptionsPreferences()
             preferences.set_constraints_json(json.dumps(constraints))
-        else:
-            preferences = None
         expected_options = expected_info['options']
         features = expected_info['features']
 
         for key, expected_value in expected_options.items():
-            actual_json = provider.get_options_json_with_preferences(key, features, preferences)
+            actual_json = provider.get_options_json_with_preferences(
+                key, features, preferences)
             actual_options = json.loads(actual_json)
             assert actual_options == expected_value, (
                 f"Options for key '{key}' with features {features} do not match "
                 f"for test suite at {expectation_path}"
             )
 
+
 def test_suites():
-    test_suites_dir = (Path(__file__) / '../../../../tests/test_suites').resolve()
+    test_suites_dir = (
+        Path(__file__) / '../../../../tests/test_suites').resolve()
     for suite in os.listdir(test_suites_dir):
         suite_path = os.path.join(test_suites_dir, suite)
         if os.path.isdir(suite_path):
