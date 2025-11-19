4c5ed9c980fccdd68455fd035c345e634d7c13e6
Justin Harris <juharris@users.noreply.github.com>
2025-11-19 15:28:33 -0500

    metadata: Correct sigs and types

--- ruby/optify2/lib/optify_ruby/options_metadata.rb
+++ ruby/optify2/lib/optify_ruby/options_metadata.rb
@@ -8,16 +8,16 @@ require_relative './base_config'
 module Optify
   # Information about a feature.
   class OptionsMetadata < FromHashable
-    sig { returns(T::Array[String]) }
+    sig { returns(T.nilable(T::Array[String])) }
     attr_reader :aliases
 
-    sig { returns(T::Array[String]) }
+    sig { returns(T.nilable(T::Array[String])) }
     attr_reader :dependents
 
     sig { returns(T.untyped) }
     attr_reader :details
 
-    sig { returns(String) }
+    sig { returns(T.nilable(String)) }
     attr_reader :name
 
     sig { returns(T.nilable(String)) }
@@ -25,5 +25,14 @@ module Optify
 
     sig { returns(T.nilable(String)) }
     attr_reader :path
+
+    sig { params(hash: T::Hash[T.untyped, T.untyped]).returns(OptionsMetadata) }
+    def self.from_hash(hash)
+      # Convert empty arrays to nil before creating the object
+      modified_hash = hash.dup
+      modified_hash['dependents'] = nil if hash['dependents'].is_a?(Array) && hash['dependents'].empty?
+      modified_hash['aliases'] = nil if hash['aliases'].is_a?(Array) && hash['aliases'].empty?
+      super(modified_hash)
+    end
   end
 end
