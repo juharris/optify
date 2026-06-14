1334b17a9e6d87ec564a28ecc20bc0bbc6113441
Justin D. Harris <juharris@users.noreply.github.com>
2026-06-14 11:15:17 -0400

    Bump versions. Patch for optimizations

--- elixir/optify/mix.exs
+++ elixir/optify/mix.exs
@@ -1,7 +1,7 @@
 defmodule Optify.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.1"
   @source_url "https://github.com/juharris/optify"
   @hex_url "https://hex.pm/packages/optify"
   @docs_url "https://hexdocs.pm/optify"
