f97c9626ae5720964be99ae322914224917264d6
juharris <1594505+juharris@users.noreply.github.com>
2026-06-22 01:28:14 +0000

    chore: bump versions (patch)

--- elixir/optify/mix.exs
+++ elixir/optify/mix.exs
@@ -1,7 +1,7 @@
 defmodule Optify.MixProject do
   use Mix.Project
 
-  @version "0.3.1"
+  @version "0.3.2"
   @source_url "https://github.com/juharris/optify"
   @hex_url "https://hex.pm/packages/optify"
   @docs_url "https://hexdocs.pm/optify"
